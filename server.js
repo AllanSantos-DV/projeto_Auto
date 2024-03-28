6000a718677ce0d414c40cf3a167fddc0d062cca
Allan Santos <allannascimentodossantos@gmail.com>
2024-03-28 11:37:44 -0300

    Estruturação dos arquivos

--- server.js
+++ server.js
@@ -6,7 +6,6 @@ const PORT = process.env.PORT || 3000;
 // Inicializa o banco de dados e o servidor HTTP
 initDb().then(() => {
     app.listen(PORT, () => {
-        console.log(`Servidor rodando na porta ${PORT}`);
         console.log(`http://localhost:${PORT}/pessoas`);
     });
 }).catch(error => {
